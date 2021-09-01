c596da04a6180cb22a7b2dcc48a88e28d2ebedd7
nina vitvinskaja <nina.vitvinskaja@ivkhk.ee>
2021-09-01 12:57:41 +0300

    Стрелочная функция

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 "use strict";
 
+
+
+
+// Интерполяция
+
 const typeGoods = 'food';
 
 //console.log('https://market.com/' + typeGoods);
 
 //console.log('https://market.com/' + typeGoods + '/' + '10');
 
-//console.log(`https://market.com/${typeGoods}`);
+console.log(`https://market.com/${typeGoods}`);
 
 
-//Задание 1 - Интерполяция
+// Задание 1 
 
 var category = 'games';
 var genre = 'strategy';
@@ -17,4 +22,32 @@ var page = '10';
 
 console.log(`https://market.com/${category}/${genre}/${page}`);
 
-//
+
+
+// Стрелочные функции
+
+let number = 10;
+
+function showMessage(text) {
+    console.log(text);
+    console.log(number);
+}
+
+showMessage("Hello!!!");
+console.log(number);
+
+const server = function() {
+    console.log('server start...');
+}
+
+server();
+
+const calc = (x, y) => x + y;
+
+console.log(calc(10, 5));
+
+
+// Задание 2
+
+const PiHello = () => "Hello! " + Math.PI;
+console.log(PiHello());
