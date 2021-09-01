22ff807c58ba3bd11421e75d8aafc2c4402c313e
nina vitvinskaja <nina.vitvinskaja@ivkhk.ee>
2021-09-01 12:43:42 +0300

    Интерполяция

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const typeGoods = 'food';
+
+//console.log('https://market.com/' + typeGoods);
+
+//console.log('https://market.com/' + typeGoods + '/' + '10');
+
+//console.log(`https://market.com/${typeGoods}`);
+
+
+//Задание 1 - Интерполяция
+
+var category = 'games';
+var genre = 'strategy';
+var page = '10';
+
+console.log(`https://market.com/${category}/${genre}/${page}`);
+
+//
