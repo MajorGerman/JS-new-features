aff84b412a678eb0a51ffc2cd2e8c06e4c1a143a
nina vitvinskaja <nina.vitvinskaja@ivkhk.ee>
2021-11-16 08:47:47 +0200

    Node Start Task 1

---
+++ Node_Start/index.js
@@ -0,0 +1,8 @@
+const obj = require('./user');
+
+console.log(obj.user);
+
+obj.sayHello();
+
+console.log("Work Dir", __dirname);
+console.log("Work File", __filename);
---
+++ Node_Start/user.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'Vasya',
+    age: 30
+}
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log('Hello!');
+    }
+}
