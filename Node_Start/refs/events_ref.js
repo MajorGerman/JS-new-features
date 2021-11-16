cf474ee412cf60e96b9927e4c9d0783c57c5a8a3
nina vitvinskaja <nina.vitvinskaja@ivkhk.ee>
2021-11-16 09:40:08 +0200

    Node Start Task 5/6

--- Node_Start/refs/events_ref.js
+++ Node_Start/refs/events_ref.js
@@ -1,21 +1,32 @@
 const EventEmitter = require('events')
 
-class Logger extends EventEmitter {
-    log(message) {
-        this.emit('message', `${message} ${Date.now()}`)
+// class Logger extends EventEmitter {
+//     log(message) {
+//         this.emit('message', `${message} ${Date.now()}`)
+//     }
+// }
+
+// const logger = new Logger()
+
+// logger.on('message', data => {
+//     console.log(data)
+// })
+
+// logger.log('Hello!')
+// logger.log('Hello!')
+// logger.log('Hello!')
+
+// logger.log('First!')
+// logger.log('Second!')
+// logger.log('Third!')
+
+
+class Nikita extends EventEmitter {
+    age = 30
+    getAge() {
+        console.log(this.age)
     }
 }
 
-const logger = new Logger()
-
-logger.on('message', data => {
-    console.log(data)
-})
-
-logger.log('Hello!')
-logger.log('Hello!')
-logger.log('Hello!')
-
-logger.log('First!')
-logger.log('Second!')
-logger.log('Third!')
+const nik = new Nikita()
+nik.getAge()
